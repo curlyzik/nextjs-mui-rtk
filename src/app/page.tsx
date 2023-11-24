014d7aa532643d245ad1363231f980530fefbd31
Isaac Nzekwe <curlyzik@gmail.com>
2023-11-24 12:38:36 +0100

    show dummy message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
+import ShowWelcomeMessage from '@/components/ShowWelcomeMessage';
 
 export default function Home() {
-    return <main>Hello</main>;
+  return (
+    <main>
+      <ShowWelcomeMessage />
+    </main>
+  );
 }
---
+++ src/components/ShowWelcomeMessage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowWelcomeMessage = () => {
+  return <div className="text-4xl">Welcome Home</div>;
+};
+
+export default ShowWelcomeMessage;
