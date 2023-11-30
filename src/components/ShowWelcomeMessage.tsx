5d27721913af77c4d4c6daab2e0ca1d80ad876fb
Isaac Nzekwe <curlyzik@gmail.com>
2023-11-30 17:13:06 +0100

    cleanup

--- src/components/ShowWelcomeMessage.tsx
+++ src/components/ShowWelcomeMessage.tsx
@@ -1,7 +1,20 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 const ShowWelcomeMessage = () => {
-  return <div className="text-4xl">Welcome Home</div>;
+  return (
+    <div className="p-4">
+      <div className="grid grid-cols-4">
+        <div>
+          <div className="font-semibold text-4xl pb-4">RTK Query
+          </div>
+          <div>
+            <Button variant="contained">Fetch Data</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShowWelcomeMessage;
