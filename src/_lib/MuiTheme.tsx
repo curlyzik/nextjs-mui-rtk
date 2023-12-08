6c776182588e8736361af21f2d7289da5c271f3a
Isaac Nzekwe <curlyzik@gmail.com>
2023-12-08 13:18:00 +0100

    updated fonts

--- src/_lib/MuiTheme.tsx
+++ src/_lib/MuiTheme.tsx
@@ -7,12 +7,7 @@ import { ThemeProvider } from '@mui/material';
 const MuiTheme: React.FC<React.PropsWithChildren> = ({ children }) => {
   const muitheme = createTheme({
     typography: {
-      fontFamily: [
-        'IBM Plex Sans',
-        'Roboto',
-        'Space Grotesk',
-        'sans-serif',
-      ].join(','), //Update fonts for MUI Components
+      fontFamily: ['Manrope'].join(','), //Update fonts for MUI Components
     },
   });
   return <ThemeProvider theme={muitheme}>{children}</ThemeProvider>;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  important: true,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,7 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Plus Jakarta Sans', 'Roboto', 'Space Grotesk', 'sans-serif'], //Update fonts
+        manrope: ['Manrope', 'sans-serif'], //Update fonts
       },
     },
   },
