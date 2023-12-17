de8b0b20c9f4808fdca16ff1894ee1a70bf26cc1
Isaac Nzekwe <curlyzik@gmail.com>
2023-12-17 20:47:27 +0100

    added features

--- src/components/Example/Features.tsx
+++ src/components/Example/Features.tsx
@@ -1,10 +1,81 @@
 import React from 'react';
 
+const features = [
+  {
+    name: 'Typescript',
+    doc: 'https://www.typescriptlang.org/',
+    usage: '',
+  },
+  {
+    name: 'Nextjs',
+    doc: 'https://nextjs.org/docs/getting-started/installation',
+    usage: '',
+  },
+  {
+    name: 'Redux Toolkit',
+    doc: 'https://redux-toolkit.js.org/introduction/getting-started',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/redux/store.ts',
+  },
+  {
+    name: 'Redux Logger',
+    doc: 'https://www.npmjs.com/package/redux-logger',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/redux/middlewares/customLogger.ts',
+  },
+  {
+    name: 'Redux Persist',
+    doc: 'https://www.npmjs.com/package/redux-persist',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/redux/cutomStorage.ts',
+  },
+  {
+    name: 'RTK Query Global Error Logger',
+    doc: 'https://redux-toolkit.js.org/rtk-query/usage/error-handling#handling-errors-at-a-macro-level',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/redux/middlewares/rtkQueryErrorLogger.ts',
+  },
+  {
+    name: 'React Toastify',
+    doc: 'https://www.npmjs.com/package/react-toastify',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/utils/displaySuccessMessage.tsx',
+  },
+  {
+    name: 'Page loader with "nextjs-toploader"',
+    doc: 'https://www.npmjs.com/package/nextjs-toploader',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/app/layout.tsx',
+  },
+  {
+    name: 'Mui Config',
+    doc: 'https://www.npmjs.com/package/nextjs-toploader',
+    usage:
+      'https://github.com/curlyzik/nextjs-mui-rtk/blob/main/src/_lib/MuiTheme.tsx',
+  },
+];
 const Features = () => {
   return (
     <div>
       <div className="font-semibold text-4xl pb-4">
-        <a href="https://github.com/curlyzik/nextjs-mui-rtk" target='_blank'>Features ⚙️</a>
+        <a
+          href="https://github.com/curlyzik/nextjs-mui-rtk"
+          target="_blank"
+          className="underline"
+        >
+          Features ⚙️
+        </a>
+      </div>
+
+      <div className="flex flex-col gap-y-5">
+        {features.map((feature) => (
+          <div key={feature.name}>
+            ✔️{' '}
+            <a href={feature.doc} className="font-semibold">
+              {feature.name}
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Example/index.tsx
+++ src/components/Example/index.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import RTK from './RTK';
 import Todo from './Todo';
 import Features from './Features';
+import { Divider } from '@mui/material';
 
 const Example = () => {
   return (
     <div className="p-6">
-      <div className="grid grid-cols-3 gap-x-12">
+      <div className="grid gap-y-7 md:grid-cols-3 gap-x-12">
         <RTK />
+        <Divider className="md:hidden" />
         <Todo />
+        <Divider className="md:hidden" />
         <Features />
       </div>
     </div>
